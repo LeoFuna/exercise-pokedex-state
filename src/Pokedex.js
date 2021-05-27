1e3fd50b0d7ef53e45b547da2acc4c269e7a3c2f
LeoFuna <leo.funa@gmail.com>
2021-05-27 19:38:43 -0300

    finalizando o grosso da pokedex

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Fragment } from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    constructor() {
-      super()
+    constructor(props) {
+      super(props)
       this.state = {
         indexOfPokemons: 0,
+        pokemons: this.props.pokemons,
       }
       this.navigationPokemonsForward = this.navigationPokemonsForward.bind(this);
       this.navigationPokemonsBackward = this.navigationPokemonsBackward.bind(this);
+      this.filtering = this.filtering.bind(this);
     }
 
     navigationPokemonsForward() {
-      if (this.state.indexOfPokemons === this.props.pokemons.length - 1) {
+      if (this.state.indexOfPokemons === this.state.pokemons.length - 1) {
         this.setState({
           indexOfPokemons: 0
         })
@@ -26,7 +29,7 @@ class Pokedex extends React.Component {
     navigationPokemonsBackward() {
       if (this.state.indexOfPokemons === 0) {
         this.setState({
-          indexOfPokemons: this.props.pokemons.length -1
+          indexOfPokemons: this.state.pokemons.length -1
         })
       } else {
         this.setState((estadoAnterior) => ({
@@ -35,13 +38,35 @@ class Pokedex extends React.Component {
       }
     }
 
+    filtering(type) {
+      const pokemonsFiltered = this.props.pokemons.filter((pokemon) => pokemon.type === type);
+      this.setState(() => ({
+        indexOfPokemons: 0,
+        pokemons: pokemonsFiltered,
+      }))
+    }
+
     render() {
+        console.log(this.state.pokemons)
         return (
+          <Fragment>
             <div className="pokedex">
-                <Pokemon key={this.props.pokemons[this.state.indexOfPokemons].id} pokemon={this.props.pokemons[this.state.indexOfPokemons]} />
-                <button onClick={ this.navigationPokemonsForward }>Frente</button>
-                <button onClick={ this.navigationPokemonsBackward }>Tras</button>
+                <Pokemon pokemon={this.state.pokemons[this.state.indexOfPokemons]} />
             </div>
+            <div>
+              <button className='btn-navi' onClick={ this.navigationPokemonsBackward }>{'<'}</button>
+              <button className='btn-navi' onClick={ this.navigationPokemonsForward }>{'>'}</button>
+            </div>
+            <div>
+              <button className='btn-filter' onClick={ () => this.filtering('Fire') }>Fire</button>
+              <button className='btn-filter' onClick={ () => this.filtering('Electric') }>Eletric</button>
+              <button className='btn-filter' onClick={ () => this.filtering('Bug') }>Bug</button>
+              <button className='btn-filter' onClick={ () => this.filtering('Poison') }>Poison</button>
+              <button className='btn-filter' onClick={ () => this.filtering('Psychic') }>Psychic</button>
+              <button className='btn-filter' onClick={ () => this.filtering('Normal') }>Normal</button>
+              <button className='btn-filter' onClick={ () => this.filtering('Dragon') }>Dragon</button>
+            </div>
+          </Fragment>
         );
     }
 }
