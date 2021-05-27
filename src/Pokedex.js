a9d377e98c3e9ab11437d970ccd65bf4adb6b4a6
LeoFuna <leo.funa@gmail.com>
2021-05-27 14:19:55 -0300

    add comando de tras e frente dos pokemons

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,45 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+      super()
+      this.state = {
+        indexOfPokemons: 0,
+      }
+      this.navigationPokemonsForward = this.navigationPokemonsForward.bind(this);
+      this.navigationPokemonsBackward = this.navigationPokemonsBackward.bind(this);
+    }
+
+    navigationPokemonsForward() {
+      if (this.state.indexOfPokemons === this.props.pokemons.length - 1) {
+        this.setState({
+          indexOfPokemons: 0
+        })
+      } else {
+        this.setState((estadoAnterior) => ({
+          indexOfPokemons: estadoAnterior.indexOfPokemons + 1
+        }))
+      }
+    }
+    
+    navigationPokemonsBackward() {
+      if (this.state.indexOfPokemons === 0) {
+        this.setState({
+          indexOfPokemons: this.props.pokemons.length -1
+        })
+      } else {
+        this.setState((estadoAnterior) => ({
+          indexOfPokemons: estadoAnterior.indexOfPokemons - 1
+        }))
+      }
+    }
+
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={this.props.pokemons[this.state.indexOfPokemons].id} pokemon={this.props.pokemons[this.state.indexOfPokemons]} />
+                <button onClick={ this.navigationPokemonsForward }>Frente</button>
+                <button onClick={ this.navigationPokemonsBackward }>Tras</button>
             </div>
         );
     }
